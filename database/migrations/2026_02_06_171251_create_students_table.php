81ce49a3009a37eeea0dd8042c7ad08604bc8580
Mostafa Nouh <mustafa.nouh.nouh@gmail.com>
2026-02-10 15:37:37 +0300

    edite studants table

--- database/migrations/2026_02_06_171251_create_students_table.php
+++ database/migrations/2026_02_06_171251_create_students_table.php
@@ -13,6 +13,21 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->enum('gender', ['male', 'female']);
+            $table->date('date_of_birth');
+            $table->string('place_of_birth');
+            $table->string('nationality');
+            $table->string('address');
+            $table->string('phone_number');
+            $table->string('mother_name');
+            $table->string('mother_phone_number');
+            $table->string('mother_email')->nullable();
+            $table->string('father_name');
+            $table->string('father_phone_number');
+            $table->string('father_email')->nullable();
+            $table->string('blood_group')->nullable();
             $table->timestamps();
         });
     }
